93d63c75f7058ae32635fda4fa059a8df78e41f2
jensedlund <edlund@speech.kth.se>
2020-10-12 22:32:43 +0200

    feat:add first batch of example files
    issue #2

--- conversions/conversions.py
+++ conversions/conversions.py
@@ -1,9 +1,9 @@
 import numpy as np
 import torch
-
 import logging
 
 # wge2wgi (Waveglow example to Waveglow infer)
+#####################################################################
 # From: 
 # from the Waveglow example format listed at
 # https://github.com/NVIDIA/waveglow
@@ -19,12 +19,13 @@ def wge2wgi(mel_example):
   mel_example = torch.autograd.Variable(mel_example.cuda())
   mel_example = torch.unsqueeze(mel_example, 0)
   #mel_example = mel_example.half() if is_fp16 else mel1
-  print("To:", mel_example.shape, type(mel_example))
+  logging.info("To:", mel_example.shape, type(mel_example))
   return mel_example
 # wgi2wge (Waveglow infer to Waveglow example )
+#####################################################################
 def wgi2wge(waveglow):
   logging.info("From:", waveglow.shape, type(waveglow))
   waveglow = torch.squeeze(waveglow, 0)
   waveglow = waveglow.cpu()
-  print("To:", waveglow.shape, type(waveglow))
+  logging.info("To:", waveglow.shape, type(waveglow))
   return waveglow
